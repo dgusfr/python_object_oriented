21942142669f5246d6f9fc200fa289cb7077ca4c
dgusfr <diegogustavo.fr@gmail.com>
2025-07-25 19:04:27 -0300

    feat(update): cide

---
+++ app.py
@@ -0,0 +1,13 @@
+from restaurant import Restaurant
+
+coco_bambu = Restaurant("Coco Bambu", "Italian")
+abbraccio = Restaurant("Abbraccio", "Mediterranean")
+barbacoa = Restaurant("Barbacoa", "Steak house")
+
+
+def main():
+    Restaurant.list_of_restaurants()
+
+
+if __name__ == "__main__":
+    main()
