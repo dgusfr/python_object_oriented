bc3d38c667c1617652482f24b23be69daeae75c5
dgusfr <diegogustavo.fr@gmail.com>
2025-07-25 18:25:14 -0300

    feat update index

--- restaurant.py
+++ restaurant.py
@@ -1,10 +1,10 @@
 class Restaurant:
     restaurants = []
 
-    def __init__(self, name, cuisine_type, status):
+    def __init__(self, name, cuisine_type):
         self.name = name
         self.cuisine_type = cuisine_type
-        self.status = status
+        self._status = False
         self.add_restaurant()
 
     def __str__(self):
@@ -17,7 +17,7 @@ class Restaurant:
     def list_of_restaurants(cls):
         for restaurant in cls.restaurants:
             print(
-                f"{restaurant.name} - {restaurant.cuisine_type} - {restaurant.status}"
+                f"{restaurant.name} - {restaurant.cuisine_type} - {restaurant.is_open}"
             )
 
     @property
@@ -25,9 +25,7 @@ class Restaurant:
         return "Open" if self.status else "Closed"
 
 
-coco_bambu = Restaurant("Coco Bambu", "Italian", "Open")
-abbraccio = Restaurant("Abbraccio", "Mediterranean", "closed")
+coco_bambu = Restaurant("Coco Bambu", "Italian")
+abbraccio = Restaurant("Abbraccio", "Mediterranean")
 
 Restaurant.list_of_restaurants()
-coco_bambu.is_open
-abbraccio.is_open
