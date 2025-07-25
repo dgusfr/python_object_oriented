ddd5aa447ccf3c0c8be140fae1ccfba0c9128369
dgusfr <diegogustavo.fr@gmail.com>
2025-07-25 17:42:06 -0300

    feat(add) another restaurant

--- restaurant.py
+++ restaurant.py
@@ -7,7 +7,17 @@ class Restaurant:
     def __str__(self):
         return f"Restaurant Name: {self.name}, Cuisine Type: {self.cuisine_type}, Status: {self.status}"
 
+    def list_of_restaurants(self):
+        for restaurant in Restaurant.restaurants:
+            print(
+                f"{restaurant.name} - {restaurant.cuisine_type} - {restaurant.status}"
+            )
+
 
 coco_bambu = Restaurant("Coco Bambu", "Italian", "Open")
+abbraccio = Restaurant("Abbraccio", "Mediterranean", "closed")
+
+restaurants = [coco_bambu, abbraccio]
+restaurants.list_of_restaurants()
 
 print(coco_bambu)
