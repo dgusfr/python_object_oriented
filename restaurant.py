16ce37cd0bd3e5ebdead60c018805edc39425105
dgusfr <diegogustavo.fr@gmail.com>
2025-07-25 17:59:30 -0300

    feat(udate): classmethod

--- restaurant.py
+++ restaurant.py
@@ -1,14 +1,21 @@
 class Restaurant:
+    restaurants = []
+
     def __init__(self, name, cuisine_type, status):
         self.name = name
         self.cuisine_type = cuisine_type
         self.status = status
+        self.add_restaurant()
 
     def __str__(self):
         return f"Restaurant Name: {self.name}, Cuisine Type: {self.cuisine_type}, Status: {self.status}"
 
-    def list_of_restaurants(self):
-        for restaurant in Restaurant.restaurants:
+    def add_restaurant(self):
+        Restaurant.restaurants.append(self)
+
+    @classmethod
+    def list_of_restaurants(cls):
+        for restaurant in cls.restaurants:
             print(
                 f"{restaurant.name} - {restaurant.cuisine_type} - {restaurant.status}"
             )
@@ -17,7 +24,4 @@ class Restaurant:
 coco_bambu = Restaurant("Coco Bambu", "Italian", "Open")
 abbraccio = Restaurant("Abbraccio", "Mediterranean", "closed")
 
-restaurants = [coco_bambu, abbraccio]
-restaurants.list_of_restaurants()
-
-print(coco_bambu)
+Restaurant.list_of_restaurants()
