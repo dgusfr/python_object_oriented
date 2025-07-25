2fc5bb1a13d6c1b705dbdb3e7d1321b418ec4ee7
dgusfr <diegogustavo.fr@gmail.com>
2025-07-25 17:10:16 -0300

    Refactor Restaurant class to include status attribute in constructor

--- restaurant.py
+++ restaurant.py
@@ -1,4 +1,10 @@
-class restaurant:
-    def __init__(self, name, cuisine_type):
+class Restaurant:
+    def __init__(self, name, cuisine_type, status):
         self.name = name
         self.cuisine_type = cuisine_type
+        self.status = status
+
+
+coco_bambu = Restaurant("Coco Bambu", "Italian", "Open")
+
+print(coco_bambu)
