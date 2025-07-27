452957918090c90d96652a5073f10c72d0ecab44
dgusfr <diegogustavo.fr@gmail.com>
2025-07-26 21:58:29 -0300

    feat(add):PDATE FRINL

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from models.restaurant import Restaurant
+from models.menu.drink import Drink
+from models.menu.dish import Dish
 
 coco_bambu = Restaurant("Coco Bambu", "Italian")
 abbraccio = Restaurant("Abbraccio", "Mediterranean")
 barbacoa = Restaurant("Barbacoa", "Steak house")
 
-abbraccio.make_assessment("João", 9.8)
+coco_bambu.make_assessment("João", 9.8)
+abbraccio.make_assessment("Maria", 5.6)
+barbacoa.make_assessment("José", 7.2)
+
+juice = Drink("Orange Juice", 5.00, "Fresh Orange Juice")
+pizza = Dish(
+    "Pizza Margherita",
+    30.00,
+    "Traditional pizza with tomato sauce, mozzarella cheese, and fresh basil.",
+)
 
 
 def main():
