dc10efbcec443c05f34ec0620bc7e39aa757c16c
dgusfr <diegogustavo.fr@gmail.com>
2025-07-26 22:02:51 -0300

    feat(update)

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ coco_bambu.make_assessment("João", 9.8)
 abbraccio.make_assessment("Maria", 5.6)
 barbacoa.make_assessment("José", 7.2)
 
-juice = Drink("Orange Juice", 5.00, "Fresh Orange Juice")
+juice = Drink("Orange Juice", 5.00)
 pizza = Dish(
     "Pizza Margherita",
     30.00,
